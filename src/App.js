6a15b9064897fbb3590fa250cb807cdde940a7d5
yly1992 <liyoung1949@gmail.com>
2019-08-27 22:30:11 -0500

    4th commit
    5th commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,16 @@ import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  var a = 1;
+  var b = 2;
+  var c = 3;
+  var e = 4;
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }

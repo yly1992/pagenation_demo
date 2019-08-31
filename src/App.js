0ca9ce7c46d860f7630b5c381cbb89df0804879d
yly1992 <liyoung1949@gmail.com>
2019-08-31 16:21:15 -0500

    fina add pagination feature
    adding pagination feature
    
    add feature pagination

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Posts from './components/Posts'
 import './App.css';
+import axios from 'axios';
+import Pagenation from './components/Pagenation';
 
 const App = () => {
   const [posts, setPosts] = useState([]);
-  var a = 1;
-  var b = 2;
-  var c = 3;
-  var e = 4;
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    }
+
+    fetchPosts();
+  }, []); 
+
+  //  Get Current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost,indexOfLastPost)
+
+  //change page
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
-    <div className="App">
+     <div className='container mt-5'>
+      <h1 className='text-primary mb-3'>My Blog</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagenation  
+      postsPerPage={postsPerPage} 
+      totalPosts={posts.length} 
+      paginate={paginate} 
+      />
     </div>
   );
 }
